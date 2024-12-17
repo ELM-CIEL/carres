fef914bad99eca6f8eb307fff7233c51eb31ebb0
Mohamed El Mokhtari <mohamed.elmokhtari26@gmail.com>
2024-12-17 11:03:23 +0100

    commit 9

--- carees/mcarree.cpp
+++ carees/mcarree.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main() {
     const int nbCarres = 4;
 
+    // Tableau statique de 4 carrés
     CCarre carres[nbCarres] = {
         CCarre(10, 20, 5),
         CCarre(20, 40, 10),
@@ -13,7 +14,7 @@ int main() {
         CCarre(40, 80, 20)
     };
 
-    cout << "Affichage des 4 carres apres initialisation:" << endl;
+    cout << "Affichage des 4 carres apres CCarre :" << endl;
     for (int i = 0; i < nbCarres; ++i) {
         cout << "Carre " << i + 1 << " : ";
         carres[i].Afficher();
@@ -38,6 +39,40 @@ int main() {
         cout << "Carre " << i + 1 << " deplacement vers l'Ouest : ";
         carres[i].Afficher();
     }
+
+
+    // Allocation dynamique avec "new"
+
+    cout << "\nTest d'allocation dynamique de memoire pour deuxieme carre :" << endl;
+    CCarre* pCarre = new CCarre(50, 50, 30);
+    cout << "Affichage du carre alloue dynamiquement : ";
+    pCarre->Afficher();
+
+
+    pCarre->Deplacer('n', 10);
+    cout << "Apres deplacement vers le Nord : ";
+    pCarre->Afficher();
+
+    pCarre->Deplacer('e', 15);
+    cout << "Apres deplacement vers l'Est : ";
+    pCarre->Afficher();
+
+    pCarre->Deplacer('s', 5);
+    cout << "Apres deplacement vers le Sud : ";
+    pCarre->Afficher();
+
+    pCarre->Deplacer('o', 20);
+    cout << "Apres deplacement vers l'Ouest : ";
+    pCarre->Afficher();
+
+    pCarre->Setsx(100);
+    pCarre->Setsy(200);
+    pCarre->Setcote(50);
+    cout << "Apres modification des attributs (sx, sy, cote) : ";
+    pCarre->Afficher();
+
+    delete pCarre;
+    cout << "Memoire du deuxieme carre liberee avec delete." << endl;
 
     return 0;
 }
